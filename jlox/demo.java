9675f7e62de27ddd257fe385da8dff74b152ed4c
Anthony Avancena-Brigante <anthonypbrigante@gmail.com>
2025-05-10 18:18:14 -0400

    Challenge 1.2

---
+++ jlox/demo.java
@@ -0,0 +1,13 @@
+package jlox;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 10; 
+        if (a > 5) {
+            System.out.println("a is greater than 5");
+        } else {
+            System.out.println("a is less than or equal to 5");
+        }
+    }
+}
